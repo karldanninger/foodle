1a08feae0a93294528c121ae6a829846e04b25fd
Karl Danninger <kdanninger@gmail.com>
2011-08-30 19:45:11 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foodle::Application.config.secret_token = 'f73b0cf530fdcbb073a760edbf1fbd91293e0106de3639d11bb6b2bb6f8c784c21ff09d93e3a3f9f8f8b8bff7fa88442df0395ff097cf98d320c589dbfee8a66'
